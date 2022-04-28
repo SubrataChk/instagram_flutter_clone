feef54e121b61d61cdf5ebf4e79d190b67ab3217
SubrataChk <subrataclash@gmail.com>
2022-04-28 20:50:06 +0600

    master

--- lib/src/global/global.dart
+++ lib/src/global/global.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter_clone/src/screen/add_post.dart';
 import 'package:instagram_flutter_clone/src/screens/feed_screen.dart';
+import 'package:instagram_flutter_clone/src/screens/search/search.dart';
 
 const homeScreenItems = [
   FeedScreenSection(),
-  Text("search"),
+  SearchSection(),
   AddPostPage(),
   Text("Feed"),
   Text("Feed"),
